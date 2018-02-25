c79b2d6fa30afe02fe04b2f24a69c1bf2152a80c
Kaelan Mikowicz <eternity01033@gmail.com>
2018-02-25 13:29:46 -0800

    accidentally used the wrong indexes from the reverse sorted list

--- groupAndBlackList.py
+++ groupAndBlackList.py
@@ -1,17 +1,24 @@
 import numpy as np
 import pandas as pd
 
-grouping = 6
-target = 168
+grouping = 3
+target = 84
 blacklist = [164, 169, 60, 102, 165, 136]
 
+mistake = pd.read_csv("Data/mistake.csv")
+for index, row in mistake.iterrows():
+    if row['Group'] < 3:
+        blacklist.append(int(row['Cell']))
+        pass
+
 table = pd.read_csv("Data/data.csv")
+badIndices =  [index for index, row in table.iterrows() if int(row['Cell']) in blacklist]
+print(badIndices)
+table = table.drop(index=badIndices)
 sort = table.sort_values("Diff", ascending=False)
-badIndices =  [index for index, row in sort.iterrows() if row['Cell'] in blacklist]
-sort.drop(index=badIndices)
 topValues = sort[:target]
-reverseTopValues = topValues.sort_values("Diff")
-print(topValues)
+reverseTopValues = table.sort_values("Diff")
+reverseTopValues = reverseTopValues[18:target+18]
 average = sum(topValues['Diff']) / grouping
 print(average)
 groups = []
@@ -19,11 +26,11 @@ for i in range(grouping):
     groups.append([])
 
 for i in range(target):
-    group = pd.Series(data=int(i % grouping), index=["Group"])
-    if int(i/grouping) % 2 is 0: 
+    group = pd.Series(data=int(i % grouping) + 3, index=["Group"])
+    if int(i) % 2 is 0: 
         groups[i % grouping].append(topValues.iloc[i].append(group))
     else:
-        groups[i % grouping].append(reverseTopValues.iloc[i].append(group))
+        groups[i % grouping].append(reverseTopValues.iloc[i - 1].append(group))
 
 for i in range(grouping):
     print("average: " + str(i))
@@ -38,5 +45,10 @@ for i in range(grouping):
 
 frame = pd.concat(groups, ignore_index=True)
 print(frame)
+print("duplicates: ?")
+print(frame.duplicated(subset="Cell"))
+frame.duplicated(subset="Cell").to_csv("Data/duplicated.csv")
 frame.to_csv("Data/grouped.csv", index=False)
-sort.to_csv("Data/sorted.csv", index=False)
+justCells = frame.drop(columns=["OCV", "TailVolt", "Diff"])
+justCells.to_csv("Data/checked.csv", index=False)
+sort.to_csv("Data/sorted2.csv", index=False)
